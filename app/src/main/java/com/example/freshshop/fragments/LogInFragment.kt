79053b66ce08a9ca748b56a4dad68a2d590d860a
doctor-blue <51161079+doctor-blue@users.noreply.github.com>
2020-02-21 10:31:01 +0700

    Complete log in screen

--- app/src/main/java/com/example/freshshop/fragments/LogInFragment.kt
+++ app/src/main/java/com/example/freshshop/fragments/LogInFragment.kt
@@ -1,14 +1,30 @@
 package com.example.freshshop.fragments
 
+import android.text.Editable
 import android.view.View
+import android.widget.Toast
 import com.example.freshshop.R
 import com.moony.calc.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_log_in.*
 
 class LogInFragment : BaseFragment() {
-    override fun init(view: View) {
-
+    override fun init() {
+        btn_sign_in.setOnClickListener {
+            if (isPasswordValid(edt_password.text)) {
+                Toast.makeText(activity, "Logged in successfully", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        edt_password.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(edt_password.text)) text_input_password.error = null
+            false
+        }
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_log_in
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 
 }
--- app/src/main/java/com/example/freshshop/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/freshshop/fragments/RegisterFragment.kt
@@ -1,11 +1,10 @@
 package com.example.freshshop.fragments
 
-import android.view.View
 import com.example.freshshop.R
 import com.moony.calc.base.BaseFragment
 
 class RegisterFragment : BaseFragment() {
-    override fun init(view: View) {
+    override fun init() {
 
     }
 
